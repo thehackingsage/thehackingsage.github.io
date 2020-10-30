684751810102bf50c3845998095c601ea3eb3d16
Mr.SAGE <thehackingsage@gmail.com>
2020-10-30 16:51:48 +0530

    new terminal

--- terminal/js/polyfills.js
+++
@@ -1,8 +0,0 @@
-/* Mr.SAGE | @thehackingsage */
-/*Add startsWith support for IE*/
-if (!String.prototype.startsWith) {
-    String.prototype.startsWith = function (searchString, position) {
-        position = position || 0;
-        return this.indexOf(searchString, position) === position;
-    };
-}
